cadb1e1dc63b136226c9e9650c7ea87a97f46d6e
grnisan <mdnisanahmed63@gmail.com>
2022-07-02 03:06:27 +0600

    text-area value empty

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  const today = new Date();
-  const date =
-    today.getDate() +
-    1 +
-    "-" +
-    (today.getMonth() + 1) +
-    "-" +
-    today.getFullYear();
+  const [todoTask, setTodoTask] = useState([]);
+
+  // const today = new Date();
+  // const date =
+  //   today.getDate() +
+  //   1 +
+  //   "-" +
+  //   (today.getMonth() + 1) +
+  //   "-" +
+  //   today.getFullYear();
 
   const handleEnterKey = (event) => {
     if (event.key === "Enter") {
-      const inputValue = event.target.value;
-      console.log(inputValue);
+      const addedTask = {
+        todo: event.target.value,
+      };
+      console.log(addedTask);
+
+      const url = `http://localhost:5000/allTodo`;
+      fetch(url, {
+        method: 'POST',
+                        headers: { 'content-type': 'application/json' },
+                        body: JSON.stringify(addedTask)
+      })
+        .then(res => res.json())
+        .then(data => {
+          event.target.value='';
+          alert("successfully added");
+        });
     }
   };
 
+  useEffect(() => {
+    const url = `https://lower-toonie-73993.herokuapp.com/service`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setTodoTask(data));
+  }, []);
+
   return (
     <div className="lg:m-12 m-8">
-
-      <h1 className="text-3xl text-green-500 my-4 text-center lg:text-left">Add your Task in Today</h1>
-
+      <h1 className="text-3xl text-green-500 my-4 text-center lg:text-left">
+        Add your Task in Today
+      </h1>
+      <p>{todoTask.length}</p>
       <textarea
         onKeyPress={handleEnterKey}
         className="textarea textarea-bordered h-24 w-full"
         name="textarea"
       ></textarea>
 
-      <label className="text-orange-400"> After completing your task just press enter key.</label>
-
+      <label className="text-orange-400">
+        {" "}
+        After completing your task just press enter key.
+      </label>
     </div>
   );
 };
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeKmTVvysR57gg0MDcfgJxijrNt1DG27o",
+  authDomain: "task-management-d2dae.firebaseapp.com",
+  projectId: "task-management-d2dae",
+  storageBucket: "task-management-d2dae.appspot.com",
+  messagingSenderId: "442495818539",
+  appId: "1:442495818539:web:a61b1306c9bbbeb6d4036e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
