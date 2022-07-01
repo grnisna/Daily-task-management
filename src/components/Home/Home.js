435394e4a2d991f194cd6e6154a5b653e0166f0a
grnisan <mdnisanahmed63@gmail.com>
2022-07-02 05:44:14 +0600

    footer done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,31 +1,31 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [todoTask, setTodoTask] = useState([]);
 
   const today = new Date();
-  const currentDate = today.getDate() +  "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
-  const nextDate = today.getDate()  + 1 +  "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
-  
-// current Day task management ************************
+  const currentDate = today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+  const nextDate = today.getDate() + 1 + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+
+  // current Day task management ************************
   const currentDayTask = (event) => {
     if (event.key === "Enter") {
-      const addedTask = {
-        todo: event.target.value,
-        date:currentDate
-      };
+        const addedTask = {
+          todo: event.target.value,
+          date: currentDate
+        };
 
-      const url = `http://localhost:5000/currentDay`;
-      fetch(url, {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify(addedTask),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          event.target.value = "";
-          alert("successfully added");
-        });
+        const url = `http://localhost:5000/currentDay`;
+        fetch(url, {
+          method: "POST",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify(addedTask),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            event.target.value = "";
+            alert("successfully added");
+          });      
     }
   };
 
@@ -34,7 +34,7 @@ const Home = () => {
     if (event.key === "Enter") {
       const addedTask = {
         todo: event.target.value,
-        date:nextDate
+        date: nextDate
       };
 
       const url = `http://localhost:5000/nextDay`;
@@ -51,50 +51,47 @@ const Home = () => {
     }
   };
 
-  useEffect(() => {
-    const url = `https://lower-toonie-73993.herokuapp.com/service`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setTodoTask(data));
-  }, []);
+
 
   return (
     <>
-    {/* Current date task management */}
-    <div className="lg:p-24 lg:my-5 p-8 bg-gray-200">
-      <h1 className="text-3xl text-green-500 my-4 text-center lg:text-left">
-        Add your Task in Today
-      </h1>
-      
-      <textarea
-        onKeyPress={currentDayTask}
-        className="textarea textarea-bordered h-24  w-full"
-        name="textarea"
-      ></textarea>
+      {/* Current date task management */}
+      <div className="lg:p-24 lg:my-5 p-8 bg-gray-200">
+        <h1 className="text-3xl text-green-500 my-4 text-center lg:text-left">
+          Add your Task in Today
+        </h1>
 
-      <label className="text-orange-400">
-        
-        After completing your task just press enter key.
-      </label>
-    </div>
+        <textarea
+          onKeyPress={currentDayTask}
+          className="textarea textarea-bordered h-24  w-full"
+          name="textarea"
+        ></textarea>
 
-    {/* Next date task management */}
-    <div className="lg:p-24 lg:mt-5 p-8 bg-gray-200">
-      <h1 className="text-3xl text-green-500 my-4 text-center lg:text-left">
-        Add your Task in {nextDate}
-      </h1>
-      
-      <textarea
-        onKeyPress={nextDayTask}
-        className="textarea textarea-bordered h-24 w-full"
-        name="textarea"
-      ></textarea>
+        <label className="text-orange-400">
 
-      <label className="text-orange-400">
-        
-        After completing your task just press enter key.
-      </label>
-    </div>
+          After completing your task just press enter key.
+        </label>
+      </div>
+      <Link to='/todo'>
+        <button className="btn btn-warning lg:w-72 w-full lg:ml-10 my-3 hover:bg-green-400 hover:border-green-400 " >See All To-Do List</button>
+      </Link>
+      {/* Next date task management */}
+      <div className="lg:p-24 lg:mt-5 p-8 bg-gray-200">
+        <h1 className="text-3xl text-green-500 my-4 text-center lg:text-left">
+          Add your Task in {nextDate}
+        </h1>
+
+        <textarea
+          onKeyPress={nextDayTask}
+          className="textarea textarea-bordered h-24 w-full"
+          name="textarea"
+        ></textarea>
+
+        <label className="text-orange-400">
+          After completing your task just press enter key.
+        </label>
+      </div>
+
     </>
   );
 };
--- src/components/TaskManagement/Todo.js
+++ src/components/TaskManagement/Todo.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Todo = () => {
+  const [currentTask, setCurrentTask] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:5000/currentDay`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCurrentTask(data));
+  }, []);
+
   return (
-    <div>
-        <h1> amar4 soner bnala</h1>
+    <div >
+      <div >
+        {
+          currentTask.map(todayTask => <div>
+            <h3 className='text-3xl'> {todayTask.date} </h3><br />
+            <textarea 
+            className='w-full h-14'
+            value={todayTask.todo}
+            ></textarea>
+          </div>)
+        }
+      </div>
     </div>
   )
 }
