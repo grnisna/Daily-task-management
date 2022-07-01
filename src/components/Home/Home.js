92fc07f29edf3d7b0dd0e50e60246a479d8d6cf6
grnisan <mdnisanahmed63@gmail.com>
2022-07-02 03:50:09 +0600

    next day planning box added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,31 +3,49 @@ import React, { useEffect, useState } from "react";
 const Home = () => {
   const [todoTask, setTodoTask] = useState([]);
 
-  // const today = new Date();
-  // const date =
-  //   today.getDate() +
-  //   1 +
-  //   "-" +
-  //   (today.getMonth() + 1) +
-  //   "-" +
-  //   today.getFullYear();
-
-  const handleEnterKey = (event) => {
+  const today = new Date();
+  const currentDate = today.getDate() +  "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+  const nextDate = today.getDate()  + 1 +  "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+  
+// current Day task management ************************
+  const currentDayTask = (event) => {
     if (event.key === "Enter") {
       const addedTask = {
         todo: event.target.value,
+        date:currentDate
       };
-      console.log(addedTask);
 
-      const url = `http://localhost:5000/allTodo`;
+      const url = `http://localhost:5000/currentDay`;
       fetch(url, {
-        method: 'POST',
-                        headers: { 'content-type': 'application/json' },
-                        body: JSON.stringify(addedTask)
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(addedTask),
       })
-        .then(res => res.json())
-        .then(data => {
-          event.target.value='';
+        .then((res) => res.json())
+        .then((data) => {
+          event.target.value = "";
+          alert("successfully added");
+        });
+    }
+  };
+
+  // next Day task management ************************
+  const nextDayTask = (event) => {
+    if (event.key === "Enter") {
+      const addedTask = {
+        todo: event.target.value,
+        date:nextDate
+      };
+
+      const url = `http://localhost:5000/nextDay`;
+      fetch(url, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(addedTask),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          event.target.value = "";
           alert("successfully added");
         });
     }
@@ -41,22 +59,43 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="lg:m-12 m-8">
+    <>
+    {/* Current date task management */}
+    <div className="lg:p-12 lg:my-5 p-8 bg-gray-200">
       <h1 className="text-3xl text-green-500 my-4 text-center lg:text-left">
         Add your Task in Today
       </h1>
-      <p>{todoTask.length}</p>
+      
       <textarea
-        onKeyPress={handleEnterKey}
+        onKeyPress={currentDayTask}
         className="textarea textarea-bordered h-24 w-full"
         name="textarea"
       ></textarea>
 
       <label className="text-orange-400">
-        {" "}
+        
         After completing your task just press enter key.
       </label>
     </div>
+
+    {/* Next date task management */}
+    <div className="lg:p-12 lg:my-5 p-8 bg-gray-200">
+      <h1 className="text-3xl text-green-500 my-4 text-center lg:text-left">
+        Add your Task in {nextDate}
+      </h1>
+      
+      <textarea
+        onKeyPress={nextDayTask}
+        className="textarea textarea-bordered h-24 w-full"
+        name="textarea"
+      ></textarea>
+
+      <label className="text-orange-400">
+        
+        After completing your task just press enter key.
+      </label>
+    </div>
+    </>
   );
 };
 
