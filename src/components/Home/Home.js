eb377ae5fdddab23a02c66e1a3cb63795e73b679
grnisan <mdnisanahmed63@gmail.com>
2022-07-01 10:31:59 +0600

    input field done and enter key press working

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>HOme page</h1>
-    </div>
-  )
-}
+  const today = new Date();
+  const date =
+    today.getDate() +
+    1 +
+    "-" +
+    (today.getMonth() + 1) +
+    "-" +
+    today.getFullYear();
 
-export default Home
+  const handleEnterKey = (event) => {
+    if (event.key === "Enter") {
+      const inputValue = event.target.value;
+      console.log(inputValue);
+    }
+  };
+
+  return (
+    <div className="lg:m-12 m-8">
+
+      <h1 className="text-3xl text-green-500 my-4 text-center lg:text-left">Add your Task in Today</h1>
+
+      <textarea
+        onKeyPress={handleEnterKey}
+        className="textarea textarea-bordered h-24 w-full"
+        name="textarea"
+      ></textarea>
+
+      <label className="text-orange-400"> After completing your task just press enter key.</label>
+
+    </div>
+  );
+};
+
+export default Home;
