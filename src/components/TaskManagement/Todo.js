27e1cd1d8c80c6672d93467f68165bbcc971e34d
grnisan <mdnisanahmed63@gmail.com>
2022-07-02 06:02:48 +0600

    todo lish added

--- src/components/TaskManagement/Todo.js
+++ src/components/TaskManagement/Todo.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 
 const Todo = () => {
   const [currentTask, setCurrentTask] = useState([]);
+  const [nextTask, setNextTask] = useState([]);
+  console.log(nextTask);
 
   useEffect(() => {
     const url = `http://localhost:5000/currentDay`;
@@ -10,15 +12,34 @@ const Todo = () => {
       .then((data) => setCurrentTask(data));
   }, []);
 
+  useEffect(() => {
+    const url = `http://localhost:5000/nextDay`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setNextTask(data));
+  }, []);
+
   return (
     <div >
-      <div >
+      <div className="lg:p-24 lg:mt-5 p-8 bg-gray-200">
         {
           currentTask.map(todayTask => <div>
-            <h3 className='text-3xl'> {todayTask.date} </h3><br />
+            <h3 className='text-3xl'>To-day Task:( {todayTask.date} ) </h3>
             <textarea 
-            className='w-full h-14'
+            className='w-full h-24 p-3'
             value={todayTask.todo}
+            ></textarea>
+          </div>)
+        }
+      </div>
+
+      <div className="lg:p-24 lg:mt-5 p-8 bg-gray-200">
+        {
+          nextTask.map(nextDayTask => <div>
+            <h3 className='text-3xl'>Next-Day Task:( {nextDayTask.date} ) </h3>
+            <textarea 
+            className='w-full h-24 p-3'
+            value={nextDayTask.todo}
             ></textarea>
           </div>)
         }
